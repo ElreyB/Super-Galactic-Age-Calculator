4475f9ad021f68c7f2d8e1010480e99642d70ffd
Elrey Belmonti <elbelmonti@gmail.com>
2017-10-13 15:07:29 -0700

    add userinterface and style index page

--- js/galactic-age-calculater-interface.js
+++ js/galactic-age-calculater-interface.js
@@ -3,6 +3,17 @@ import { GalacticAgeCalculater } from './../js/galactic-age-calculater.js';
 $(document).ready(function(){
   $('.age-form').submit(function(e){
     e.preventDefault();
-    const ageInput = 
-  })
+    const ageInput = parseInt($('input#age').val());
+    const galacticAgeCalculater = new GalacticAgeCalculater();
+
+    $('.earth-seconds').text(galacticAgeCalculater.secondsOnEarth(ageInput));
+
+    $('.mercury-years').text(galacticAgeCalculater.yearsOnMercury(ageInput));
+
+    $('.venus-years').text(galacticAgeCalculater.yearsOnVenus(ageInput));
+
+    $('.mars-years').text(galacticAgeCalculater.yearsOnMars(ageInput));
+
+    $('.jupiter-years').text(galacticAgeCalculater.yearsOnJupiter(ageInput));
+  });
 });
